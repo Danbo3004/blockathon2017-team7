288f64d258183b013c7140104146692197dea207
Tran Nguyen Anh Minh <trannguyenanhminh@Trans-MacBook-Pro.local>
2017-11-25 01:14:17 +0700

    update transfermoney.sol

--- smart_contract/contracts/TransferMoney.sol
+++ smart_contract/contracts/TransferMoney.sol
@@ -1,25 +1,46 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.11;
+contract TransferMoney {
+    uint256 public totalSupply;
+    uint256 public availableNumber;
+    mapping (address => uint256) balances;
 
-contract Owner {
-  address public owner;
-  mapping (address => uint) balances;
-
-  function Owner() {
-    owner = msg.sender;
-    balances[owner] = 9999;
-  }
-
-  function transfer(address _to, uint _value) returns (bool success) {
-    if (balances[msg.sender] < _value) {
-      return false;
+    address owner;
+    uint price = 100; // 1 ETH = 100 Token
+    address public admin;
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+    
+    modifier onlyOwner(address _addr) {
+        require(_addr == owner);
+        _;
+    }
+    
+    function Token(uint256 _totalSupply) public {
+        totalSupply = _totalSupply;
+        availableNumber = _totalSupply;
+        owner = msg.sender;
     }
 
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    return true;
-  }
+    function () payable public {
+        uint256 amount = msg.value * price / 1 ether;
+        if (amount > availableNumber) {
+            amount = availableNumber;
+        } 
 
-  function getBalance(address _user) constant returns (uint balance) {
-    return balances[_user];
-  }
+        require(amount > 0);
+
+        balances[msg.sender] += amount;
+        availableNumber -= amount;
+
+        uint cost = amount*1 ether/price;
+        msg.sender.transfer(msg.value - cost);
+        
+        admin.transfer(cost);
+
+        Transfer(0x0, msg.sender, amount);
+    }
+    function balanceOf(address _addr) public constant returns (uint256) {
+        return balances[_addr];
+    }
+
 }
