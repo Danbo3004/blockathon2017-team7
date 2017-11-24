742dcbd46bc070731bc95aac62c03f80efb871e1
doannx <doannx@gmail.com>
2017-11-25 00:32:20 +0700

    Add BookingList contract.

---
+++ smart_contract/contracts/BookingList.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.14;
+
+contract BookingList {
+
+  struct BookingInfo {
+    // user info
+    bytes32 userId;
+    // room properties
+    bool isBreakfast;
+    bool isWindowsView;
+    bool isSmoking;
+    bool isFreeCancellation;
+    uint numOfSingleBeds;
+    uint numOfDoubleBeds;
+    uint numOfAdults;
+    uint numOfChildren;
+    uint area;
+    // booking info
+    bytes32 fromDate;
+    bytes32 toDate;
+    // price
+    bytes32 finalPrice;
+  }
+    
+  mapping (bytes32 => BookingInfo) bookingList;
+
+  function BookingList() {
+  }
+
+  function addNewBooking(
+                        // user info
+                        bytes32 userId,
+                        bool isBreakfast, bool isWindowsView, bool isSmoking, bool isFreeCancellation,
+                        uint numOfSingleBeds, uint numOfDoubleBeds, uint numOfAdults, uint numOfChildren, uint area,
+                        // booking info
+                        bytes32 fromDate, bytes32 toDate,
+                        // price
+                        bytes32 finalPrice) public returns (bytes32 bookingId) 
+  {
+    var bi = BookingInfo(userId, isBreakfast, isWindowsView, isSmoking, isFreeCancellation, numOfSingleBeds, numOfDoubleBeds, numOfAdults, numOfChildren, area, fromDate, toDate, finalPrice);
+    bookingId = keccak256(bi);
+    bookingList[bookingId] = bi;
+    return bookingId;
+  }
+
+  function getBookingInfoByKey(bytes32 key) public constant returns (
+                                                              bytes32, 
+                                                              bool, bool, bool, bool, 
+                                                              uint, uint, uint, uint, uint,
+                                                              bytes32, bytes32, bytes32) 
+  {
+    return (
+      bookingList[key].userId,
+      bookingList[key].isBreakfast,
+      bookingList[key].isWindowsView,
+      bookingList[key].isSmoking,
+      bookingList[key].isFreeCancellation,
+      bookingList[key].numOfSingleBeds,
+      bookingList[key].numOfDoubleBeds,
+      bookingList[key].numOfAdults,
+      bookingList[key].numOfChildren,
+      bookingList[key].area,
+      bookingList[key].fromDate,
+      bookingList[key].toDate,
+      bookingList[key].finalPrice
+    );
+  }
+}
