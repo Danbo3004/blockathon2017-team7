f4730f3213e8b087c1d6586ec7b4ff28f21ffe4d
doannx <doannx@gmail.com>
2017-11-25 01:38:45 +0700

    Update BookingList contract

--- smart_contract/contracts/BookingList.sol
+++ smart_contract/contracts/BookingList.sol
@@ -1,54 +1,19 @@
-pragma solidity ^0.4.14;
+pragma solidity ^0.4.15;
 
 contract BookingList {
-
-  struct BookingInfo {
-    // user info
-    bytes32 userId;
-    // room properties
-    bool isBreakfast;
-    bool isWindowsView;
-    bool isSmoking;
-    bool isFreeCancellation;
-    uint numOfSingleBeds;
-    uint numOfDoubleBeds;
-    uint numOfAdults;
-    uint numOfChildren;
-    uint area;
-    // booking info
-    bytes32 fromDate;
-    bytes32 toDate;
-    // price
-    uint256 finalPrice;
-    // wallet address of referee
-    address refereeAddr;
-  }
-    
-  mapping (bytes32 => BookingInfo) bookingList;
 
   function BookingList() {
   }
 
-  function addNewBooking (
-                        // user info
-                        bytes32 userId,
-                        bool isBreakfast, bool isWindowsView, bool isSmoking, bool isFreeCancellation,
-                        uint numOfSingleBeds, uint numOfDoubleBeds, uint numOfAdults, uint numOfChildren, uint area,
-                        // booking info
-                        bytes32 fromDate, bytes32 toDate,
-                        // price
-                        uint256 finalPrice,
-                        // wallet address of referee
-                        address refereeAddr
-                        ) payable public returns (bytes32 bookingId) 
-  {
+  mapping (bytes32 => string) bookingList;
+
+  function addNewBooking(string newBooking, uint256 finalPrice, address refereeAddr) payable public returns (bytes32 bookingId) {
     if (msg.value < finalPrice) {
       return "";
     }
     // save new booking
-    var bi = BookingInfo(userId, isBreakfast, isWindowsView, isSmoking, isFreeCancellation, numOfSingleBeds, numOfDoubleBeds, numOfAdults, numOfChildren, area, fromDate, toDate, finalPrice, refereeAddr);
-    bookingId = keccak256(bi);
-    bookingList[bookingId] = bi;
+    bookingId = keccak256(newBooking);
+    bookingList[bookingId] = newBooking;
     
     // transfer placeholder money to referee
     refereeAddr.transfer(msg.value);
@@ -56,29 +21,7 @@ contract BookingList {
     return bookingId;
   }
 
-  function getBookingInfoByKey(bytes32 key) public constant returns (
-                                                              bytes32, 
-                                                              bool, bool, bool, bool, 
-                                                              uint, uint, uint, uint, uint,
-                                                              bytes32, bytes32, 
-                                                              uint256,
-                                                              address) 
-  {
-    return (
-      bookingList[key].userId,
-      bookingList[key].isBreakfast,
-      bookingList[key].isWindowsView,
-      bookingList[key].isSmoking,
-      bookingList[key].isFreeCancellation,
-      bookingList[key].numOfSingleBeds,
-      bookingList[key].numOfDoubleBeds,
-      bookingList[key].numOfAdults,
-      bookingList[key].numOfChildren,
-      bookingList[key].area,
-      bookingList[key].fromDate,
-      bookingList[key].toDate,
-      bookingList[key].finalPrice,
-      bookingList[key].refereeAddr
-    );
+  function getBookingInfoByKey(bytes32 key) public constant returns (string) {
+    return bookingList[key];
   }
 }
