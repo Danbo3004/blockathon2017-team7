24a1a1a9368a4812ac73b18c9030b51ecbfbbc25
Bill Tuan <baotuan0.0@gmail.com>
2017-11-25 21:43:34 +0700

    Config setting node

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // host: 'http://localhost:8545/',
+    host: 'http://www.blockathon.asia:8545/',
+    address: '0x1aD833a0B060F6730e36295482C6BD3A84DC659F',
+    // receiving_addr: '0xE37B1b2598F96F781173FD02B9212e513B11210D',
+    private_key: '52f13c2fe2f99e53588eed126380d2e84d02ef867b0b0fce2df40a9de2a534b7',
+    // key_store: '{"version":3,"id":"ff541ab6-939b-4074-9c62-d5a55beb80d3","address":"e37b1b2598f96f781173fd02b9212e513b11210d","crypto":{"ciphertext":"6c6d21e4861e702a6e6061e7665b678ab994e2e9028228b550dcaa7e60b6eb97","cipherparams":{"iv":"cec310e95bcaff42846b00bf6726fe32"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"43483cfd9f554dec53497b29df57c64517972ee8e46107fd4b192ccebb1f6c2e","n":1024,"r":8,"p":1},"mac":"fb422f985ce8ead4ae6f27430315e5e7a8cab97df3325aefe78bc0783d85e889"}}',
+    // key_store_pw: '123456789',
+    contract_abi: [],
+    contract_address: ''
+}
