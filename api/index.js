6749cb12c2ea415164d087df8fb69c6354cea64b
doannx <doannx@gmail.com>
2017-11-25 10:50:27 +0700

    Update contract configuration.

--- api/index.js
+++ api/index.js
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 9999;
 
 const web3 = new Web3(new Web3.providers.HttpProvider(configs.host));
 
-const bookingContractAbi = '';
-const bookingContractAddr = '';
+const bookingContractAbi = '[ { "constant": false, "inputs": [ { "name": "roomId", "type": "bytes32" }, { "name": "rate", "type": "uint256" }, { "name": "comment", "type": "bytes32" } ], "name": "addNewReview", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "roomId", "type": "bytes32" } ], "name": "getCommentsByRoomId", "outputs": [ { "name": "rateArray", "type": "uint256[]" }, { "name": "commentArray", "type": "bytes32[]" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "key", "type": "bytes32" } ], "name": "getBookingInfoByKey", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "newBooking", "type": "string" }, { "name": "finalPrice", "type": "uint256" }, { "name": "refereeAddr", "type": "address" } ], "name": "addNewBooking", "outputs": [ { "name": "bookingId", "type": "bytes32" } ], "payable": true, "type": "function" }, { "inputs": [], "payable": false, "type": "constructor" } ]';
+const bookingContractAddr = '0x16C4CEEC682B370C0A4a29F4bEDCb90d8De62450';
 
 const transferContractAbi = '';
 const transferContractAddr = '';
 
-// const bookingListContract = web3.eth.contract(bookingContractAbi).at(bookingContractAddr);
+const bookingListContract = web3.eth.contract(JSON.parse(bookingContractAbi)).at(bookingContractAddr);
 // const transferContract = web3.eth.contract(transferContractAbi).at(transferContractAddr);
 
 app.use(bodyParser.json());
@@ -27,6 +27,10 @@ app.post('/addNewBooking', (req, res) => {
     // check balance
 
     // call to addNewBooking
+    bookingListContract.getBookingInfoByKey('123456', { from: '0xF026bef6694D16c4F8dF3E5279Ab8801E8B2B4dd', gas: 500000 }, (error, info) => {
+        console.log('Info: ', info.toString(10))
+    });
+
     res.send('addNewBooking');
 });
 
@@ -37,6 +41,7 @@ app.post('/addReview', (req, res) => {
 
 app.post('/transferMoney', (req, res) => {
     console.log('transferMoney');
+    // call to transferMoney
 });
 
 app.get('/roomList', (req, res) => {
