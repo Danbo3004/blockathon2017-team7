58aaa4e3de4411120de465eaa552425ab46a371e
doannx <doannx@gmail.com>
2017-11-25 07:51:30 +0700

    Update API list.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 
-const PORT = process.env.PORT || 8888;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/addNewBooking', (req, res) => {
+    console.log('addNewBooking');
+    res.send('addNewBooking');
+});
+
+app.post('/addReview', (req, res) => {
+    console.log('addReview');
+});
+
+app.post('/transferMoney', (req, res) => {
+    console.log('transferMoney');
+});
+
+app.get('/roomList', (req, res) => {
+    console.log('roomList');
+});
+
+app.get('/login', (req, res) => {
+    console.log('login');
+});
+
+const PORT = process.env.PORT || 9999;
 app.listen(PORT);
+
+console.log('todo list RESTful API server started on: ' + PORT);
+
