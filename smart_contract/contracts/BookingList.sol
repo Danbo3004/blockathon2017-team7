488067bc5efc74176cc836ff18beb6fe57c7cc6f
doannx <doannx@gmail.com>
2017-11-25 15:57:23 +0700

    Update contract.

--- smart_contract/contracts/BookingList.sol
+++ smart_contract/contracts/BookingList.sol
@@ -3,47 +3,25 @@ pragma solidity ^0.4.15;
 contract BookingList {
 
   struct Review {
+    bytes32 roomId;
     uint rate;
     bytes32 comment;
   }
 
-  function BookingList() {
+  bytes32[] public bookingList;
+  Review[] public reviews;
+
+  function addNewBooking(bytes32 newBooking) public returns (uint256) {
+    bookingList.push(newBooking);
+    return bookingList.length;
   }
 
-  mapping (bytes32 => string) bookingList;
-
-  mapping (bytes32 => Review[]) reviews;
-
-  function addNewBooking(string newBooking, uint256 finalPrice, address refereeAddr) payable public returns (bytes32 bookingId) {
-    if (msg.value < finalPrice) {
-      return "";
-    }
-    // save new booking
-    bookingId = keccak256(newBooking);
-    bookingList[bookingId] = newBooking;
-    
-    // transfer placeholder money to referee
-    refereeAddr.transfer(msg.value);
-    
-    return bookingId;
+  function getBookingInfoByKey(uint256 index) public constant returns (bytes32) {
+    return bookingList[index];
   }
 
-  function getBookingInfoByKey(bytes32 key) public constant returns (string) {
-    return bookingList[key];
-  }
-
-  function addNewReview(bytes32 roomId, uint rate, bytes32 comment) public {
-    var roomComments = reviews[roomId];
-    roomComments.push(Review(rate, comment));
-    reviews[roomId] = roomComments;
-  }
-
-  function getCommentsByRoomId(bytes32 roomId) public constant returns (uint[] rateArray, bytes32[] commentArray) {
-    var roomComments = reviews[roomId];
-    for(uint i = 0; i < roomComments.length; i++) {
-      rateArray[i] = roomComments[i].rate;
-      commentArray[i] = roomComments[i].comment;
-    }
-    return (rateArray, commentArray);
+  function addNewReview(bytes32 roomId, uint rate, bytes32 comment) public returns (uint256) {
+    reviews.push(Review(roomId, rate, comment));
+    return reviews.length;
   }
 }
